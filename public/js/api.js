a27b431f2899ef637956e998c2fa0efc1526cb54
spiedra <jcspiedra@gmail.com>
2021-12-23 00:11:08 -0600

    feat: add get methods in api

--- public/js/api.js
+++ public/js/api.js
@@ -1,7 +1,74 @@
 // AJAX -> jQuery
 
+// GET
+
+const getMoviesJquery = () => {
+  $.ajax({
+    url: "https://muscid-buses.000webhostapp.com/lavarel-RESTful-api/public/api/movies",
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+    },
+  });
+};
+
+const getGenresJquery = () => {
+  $.ajax({
+    url: "https://muscid-buses.000webhostapp.com/lavarel-RESTful-api/public/api/genres",
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+    },
+  });
+};
+
+// PUT
+
+// UPDATE
+
+// DELETE
+
 // -----------------------------------------------------------------------------------
 
 // AJX -> Vanilla JS
 
 // -----------------------------------------------------------------------------------
+
+// GET
+
+const getMoviesVanillaJs = () => {
+  fetch(
+    "https://muscid-buses.000webhostapp.com/lavarel-RESTful-api/public/api/movies",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+const getGenresVanillaJs = () => {
+  fetch(
+    "https://muscid-buses.000webhostapp.com/lavarel-RESTful-api/public/api/genres",
+    {
+      method: "GET",
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+};
+
+// Request
+
+// getMoviesJquery();
+// getGenresJquery();
+getMoviesVanillaJs();
+getGenresVanillaJs();
--- public/js/main.jquery.js
+++ public/js/main.jquery.js
@@ -1,3 +1,11 @@
+/*
+  JavaScript Variables:
+    * var: globally-scoped variable | can be Reassigned | can be Redeclare
+    * let: cannot be Redeclared | must be Declared before use | has Block Scope
+    * const: cannot be Redeclared | cannot be Reassigned | has Block Scope
+  Resource: https://www.w3schools.com/js/js_variables.asp 
+*/
+
 // DOM Manipulation -> JQuery
 
 // -----------------------------------------------------------------------------------
