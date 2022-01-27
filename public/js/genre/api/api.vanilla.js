5115e429d1eefd881f41d2c079eb5816bc619438
spiedra <jcspiedra@gmail.com>
2022-01-26 22:25:43 -0600

    docs: add some comments

---
+++ public/js/genre/api/api.vanilla.js
@@ -0,0 +1,5 @@
+/*
+  Docs: https://www.w3schools.com/jquery/ajax_ajax.asp
+  ---
+  * Una función que pasa como arguemento a otra función, se llama callback function
+*/
---
+++ public/js/genre/main.vanilla.js
@@ -0,0 +1,4 @@
+/*
+  Differences between forEach() and map() methods:
+    * https://www.geeksforgeeks.org/difference-between-foreach-and-map-loop-in-javascript/
+*/
